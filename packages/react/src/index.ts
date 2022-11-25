50becd71e6f6bd49f49d83e15a1905de30a97419
Mathews A <mathews.mw@gmail.com>
2022-11-24 21:44:21 -0400

    A base de um Design System - Configuração do ESLint

---
+++ packages/react/src/index.ts
@@ -0,0 +1,7 @@
+import { colors } from '@ignite-ui/tokens/src/colors'
+
+const colorBlack = colors.black
+
+const colorWhite = colors.white
+
+console.log(colorBlack, colorWhite)
---
+++ packages/tokens/src/radii.ts
@@ -0,0 +1,10 @@
+// padrões de border-radius
+
+export const radii = {
+  px: '1px',
+  xs: '4px',
+  sm: '6px',
+  md: '8px',
+  lg: '16px',
+  full: '99999px',
+} as const
